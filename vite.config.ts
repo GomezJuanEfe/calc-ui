7986b0556a7402907b7a8e57da9bb7e67d015f75
Juan Gomez <jf.gomezramirez@gmail.com>
2023-10-20 15:32:47 -0500

    Server config on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gomezjuanefe.github.io/"
+  server: {
+    host: "https://gomezjuanefe.github.io/calc-ui/"
+  },
+  preview: {
+    host: true
+  }
 })
